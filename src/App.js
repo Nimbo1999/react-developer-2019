1c0f83f8bce6bddcaac924dc8644dee830ee0dc2
Matheus de Oliveira Lopes <matlopes1999@gmail.com>
2019-10-11 01:05:53 -0300

    Primeiros ajustes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      monsters: [
+        {
+          name: 'Dracula',
+          id: 'monster1'
+        },
+        {
+          name: 'Zombie',
+          id: 'monster2'
+        },
+        {
+          name: 'Frankenstein',
+          id: 'monster3'
+        },
+      ]
+    };
+  }
+
+  render(){
+    return (
+      <div className="App">
+        {
+          this.state.monsters.map(monster => <h3 key={monster.id}>{ monster.name }</h3>)
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
