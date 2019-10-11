8c419ff0ca8318c69de398dcce614adf6782751d
Matheus de Oliveira Lopes <matlopes1999@gmail.com>
2019-10-11 17:18:46 -0300

    add card-list Component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import { CardList } from './components/card-list/card-list.component'
+
 class App extends Component {
 
   constructor(){
     super();
     this.state = {
-      monsters: [
-        {
-          name: 'Dracula',
-          id: 'monster1'
-        },
-        {
-          name: 'Zombie',
-          id: 'monster2'
-        },
-        {
-          name: 'Frankenstein',
-          id: 'monster3'
-        },
-      ]
+      monsters: []
     };
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then( response => response.json() )
+    .then(users => this.setState({ monsters: users }))
   }
 
   render(){
     return (
       <div className="App">
-        {
-          this.state.monsters.map(monster => <h3 key={monster.id}>{ monster.name }</h3>)
-        }
+        <CardList name="Matheus">
+          {
+            this.state.monsters.map(monster => <h3 key={monster.id}>{ monster.name }</h3>)
+          }
+        </CardList>
       </div>
     );
   }
