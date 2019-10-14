3e289eb9518252bb48e6e92f9cf1f506dddab2fe
Matheus de Oliveira Lopes <matlopes1999@gmail.com>
2019-10-13 21:01:59 -0300

    Updates

---
+++ precache-manifest.6fc25ac047975e33242ae0064280fc3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53c07c254f03f4a6cf150efdbcf9785b",
+    "url": "/react-developer-2019/index.html"
+  },
+  {
+    "revision": "4898cb7be3811aa75982",
+    "url": "/react-developer-2019/static/css/main.10a862e0.chunk.css"
+  },
+  {
+    "revision": "5ffea8f1ef1d054a3e16",
+    "url": "/react-developer-2019/static/js/2.1216e14e.chunk.js"
+  },
+  {
+    "revision": "4898cb7be3811aa75982",
+    "url": "/react-developer-2019/static/js/main.4704b701.chunk.js"
+  },
+  {
+    "revision": "0ef5fbee03d054446946",
+    "url": "/react-developer-2019/static/js/runtime-main.8c8be790.js"
+  }
+]);
